2acc7dd5df5b0708331ff20be53942668d622bea
LeDuoc95 <54106132+LeDuoc95@users.noreply.github.com>
2020-09-15 22:29:32 +0700

    15_09_2020

--- assets/js/rockPaper.js
+++ assets/js/rockPaper.js
@@ -4,6 +4,10 @@ const handRock = document.getElementById("hand-rock");
 const gameMasseges = document.getElementById("game-masseges");
 const gameScore = document.getElementById("game-score");
 const loader = document.getElementById("loader");
+const loseOrWin = document.getElementById('lose-or-win');
+const nodeChoiceOfUser = document.getElementById('choice-of-user');
+const nodeChoiceOfComp = document.getElementById('choice-of-comp');
+const resultOfPlay = document.getElementById('result');
 
 const addListenerRockHand = () => {
   handPaper.addEventListener("click", () => playGame(1));
@@ -11,30 +15,64 @@ const addListenerRockHand = () => {
   handRock.addEventListener("click", () => playGame(3));
 };
 
-const playGame = async (userOfChoise) => {
-  let winner = "balance";
-  // loader.style.display = "flex";
-  let handOfComputer = await new Promise((resolve, reject) =>
+const playGame = async (choiceOfUser) => {
+  let winner = "Duplicate!!!";
+  loader.style.display = "flex";
+  let choiceOfComputer = await new Promise((resolve, reject) =>
     setTimeout(() => {
       resolve(Math.floor(Math.random() * 3) + 1);
     }, 3000)
   );
-  // loader.style.display = "none";
-  console.log("userOfChoise", handOfComputer);
+  const core = choiceOfUser - choiceOfComputer;
+  loader.style.display = "none";
 
-  // if (handOfComputer === 1) {
-  //   handOfComputer = "paper";
-  // }
+  if (choiceOfComputer === 1) {
+    choiceOfComputer = "paper";
+  }
 
-  // if (handOfComputer === 2) {
-  //   handOfComputer = "scissor";
-  // }
+  if (choiceOfComputer === 2) {
+    choiceOfComputer = "scissor";
+  }
 
-  // if (handOfComputer === 3) {
-  //   handOfComputer = "rock";
-  // }
+  if (choiceOfComputer === 3) {
+    choiceOfComputer = "rock";
+  }
 
-  if (userOfChoise - handOfComputer === 0) {
+  if (choiceOfUser === 1) {
+    choiceOfUser = "paper";
+  }
+
+  if (choiceOfUser === 2) {
+    choiceOfUser = "scissor";
+  }
+
+  if (choiceOfUser === 3) {
+    choiceOfUser = "rock";
+  }
+  
+  if (core === -1 || core === 2) {
+    winner = "You Lose!!!"
+  }
+
+  if (core === 1 || core === -2) {
+    winner = "You Win!!!"
+  }
+
+  loseOrWin.innerHTML = winner;
+  nodeChoiceOfUser.innerHTML = choiceOfUser;
+  nodeChoiceOfComp.innerHTML = choiceOfComputer;
+  resultOfPlay.innerHTML = `${core === -1 || core === 2 ? 'lose': core === 1 || core === -2 ? "win":"duplicate"}`;
+  gameMasseges.style.visibility = "visible";
+  let totalGameScore = gameScore.innerHTML.split(' : ');
+
+  if(winner === "You Win!!!") {
+    totalGameScore[0] = parseInt(totalGameScore[0]) + 1;
+    gameScore.innerHTML = totalGameScore.join(' : ');
+  }
+
+  if(winner === "You Lose!!!") {
+    totalGameScore[1] = parseInt(totalGameScore[1]) + 1;
+    gameScore.innerHTML = totalGameScore.join(' : ');
   }
 };
 
